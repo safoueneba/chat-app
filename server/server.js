4c1b979c8fbe9f2d834ecad58b2a56deffb0cca5
safouene <benalaya.safouene.com>
2017-06-11 01:27:40 +0100

    first commit chat-app

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+var express = require('express');
+var port = process.env.PORT || 3000;
+
+
+
+var app = express();
+
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(port, () => {
+		console.log('server started in port '+port);
+});
